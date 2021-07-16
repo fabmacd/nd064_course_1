b1b26d563ad3bfb5c58bb9a666e4209772066999
fabmacd <fabmacd@yahoo.com.br>
2021-07-15 23:32:48 -0300

    Update app.py

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,8 +1,38 @@
 from flask import Flask
+from flask import json
+import logging
+
 app = Flask(__name__)
+
+@app.route("/staus")
+def status():
+    response = app.response_class(
+        response=json.dumps({"result":"OK - health"}),
+        status=200,
+        mimetype='application/json'
+    )
+    ## log line status
+    app.logger.info('status request successful')
+    
+    returne response
+    
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+        response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+        status=200,
+        mimetype='application/json'
+    )
+    ## log line metrics
+    app.logger.info('Metrics request successful')
+    
+    return response
 
 @app.route("/")
 def hello():
+    ## log line main
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+    
     return "Hello World!"
 
 if __name__ == "__main__":
